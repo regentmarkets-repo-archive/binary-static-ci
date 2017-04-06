27931ed196e32969316259661c1f7292d713a1f4
ridho <rijuve@yahoo.com>
2017-04-06 16:24:25 +0800

    indentation

--- src/test/java/pageObjects/Login_History.java
+++ src/test/java/pageObjects/Login_History.java
@@ -8,33 +8,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Login_History {
-	private static WebElement element = null;
+    private static WebElement element = null;
+    //Method to find element of Login History
+    public static WebElement title_page(WebDriver driver) {
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login_history-title")));
+        Assert.assertTrue(element.isDisplayed());
+        return element;
+    }
+    //Method to calculate number of records in login history 
+    public static int count_rows(WebDriver driver){
+        int element= driver.findElements(By.xpath("//*[@id='login-history-table']/tbody/tr[*]")).size();
+        return element;
+    }
 	
-	//Method to find element of Login History
-	
-	public static WebElement title_page(WebDriver driver) {
-		WebDriverWait wait = new WebDriverWait(driver,30);
-		element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login_history-title")));
-		Assert.assertTrue(element.isDisplayed());
-		return element;
-	}
-	//Method to calculate number of records in login history 
-	public static int count_rows(WebDriver driver){
-    int element= driver.findElements(By.xpath("//*[@id='login-history-table']/tbody/tr[*]")).size();
-    return element;
-	}
-	
-	public static WebElement first_row(WebDriver driver){
-	WebDriverWait wait = new WebDriverWait(driver,30);
-	element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='login-history-table']/tbody/tr[1]/td[5]")));
-    Assert.assertTrue(element.isDisplayed());
-	return element;
-	}
-	//Method to find second rows of the records 
-	public static WebElement second_row(WebDriver driver){
-		WebDriverWait wait = new WebDriverWait(driver,30);
-		element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='login-history-table']/tbody/tr[2]/td[5]")));
-		Assert.assertTrue(element.isDisplayed());
-		return element;
-		}
+    public static WebElement first_row(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='login-history-table']/tbody/tr[1]/td[5]")));
+        Assert.assertTrue(element.isDisplayed());
+        return element;
+    }
+    //Method to find second rows of the records 
+    public static WebElement second_row(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='login-history-table']/tbody/tr[2]/td[5]")));
+        Assert.assertTrue(element.isDisplayed());
+        return element;
+    }
 }
--- src/test/java/pageObjects/Security_Page.java
+++ src/test/java/pageObjects/Security_Page.java
@@ -8,14 +8,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class Security_Page {
-	private static WebElement element = null;
-	//Method to find Login History link
-	public static WebElement link_LoginHistoryPage(WebDriver driver)
-	{
-		WebDriverWait wait = new WebDriverWait(driver,10);
-		element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='settings_container']/div/div[5]/div[1]/a")));
-		Assert.assertTrue(element.isDisplayed());
-		return element;
-	}
+    private static WebElement element = null;
+    //Method to find Login History link
+    public static WebElement link_LoginHistoryPage(WebDriver driver)
+    {
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='settings_container']/div/div[5]/div[1]/a")));
+        Assert.assertTrue(element.isDisplayed());
+        return element;
+    }
 }
-
