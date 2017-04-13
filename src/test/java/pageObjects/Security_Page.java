661aca1d398e2cc7ce64f1e1ece579e3ed8a4b19
Ng Chen Hon <chenhon@binary.com>
2017-04-13 17:33:37 +0800

    self exclusion check input validation

--- src/test/java/pageObjects/Security_Page.java
+++ src/test/java/pageObjects/Security_Page.java
@@ -17,4 +17,13 @@ public class Security_Page {
         Assert.assertTrue(element.isDisplayed());
         return element;
     }
+    
+    //Method to find Self Exclusion link
+    public static WebElement link_SelfExclusionPage(WebDriver driver)
+    {
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='settings_container']/div/div[3]/div[1]/a")));
+        Assert.assertTrue(element.isDisplayed());
+        return element;
+    }
 }
