783a1fbda808ecfdb298b29d06c1fedc23076721
ridho <rijuve@yahoo.com>
2017-04-13 10:03:05 +0800

    Initial Add Authorised Application Test Case

--- src/test/java/pageObjects/Security_Page.java
+++ src/test/java/pageObjects/Security_Page.java
@@ -24,7 +24,14 @@ public class Security_Page {
         Assert.assertTrue(element.isDisplayed());
         return element;
     }
-  //Method to find Account Password link
+  //Method to find Authorized Application link
+    public static WebElement link_AuthorisedApplication(WebDriver driver)
+    {
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='change_password']/div[2]/h4/a")));
+        Assert.assertTrue(element.isDisplayed());
+        return element;
+    }
     public static WebElement link_AccountPassword(WebDriver driver)
     {
         WebDriverWait wait = new WebDriverWait(driver,10);
