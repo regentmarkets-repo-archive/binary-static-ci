7e56279c6088d74fef611a94b693d60de731bfe2
ridho <rijuve@yahoo.com>
2017-04-24 13:16:59 +0800

    Change to verify links

--- src/test/java/appModules/VerifyLinks_Action.java
+++ src/test/java/appModules/VerifyLinks_Action.java
@@ -3,18 +3,13 @@ package appModules;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import pageObjects.Footer_Menu;
 
 public class VerifyLinks_Action {
     //Method to check title page 
 
-    public static void ExecuteCount(WebDriver driver){
-        System.out.println("Total Footer Menu: " + Footer_Menu.footer(driver));
-    }
     //Method to get all links list
     public static void ListLinks(WebDriver driver ,List<WebElement> verifylink){
         List<WebElement> links=verifylink;
--- src/test/java/testCases/Validate_Links.java
+++ src/test/java/testCases/Validate_Links.java
@@ -4,7 +4,7 @@ package testCases;
 
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
-import pageObjects.Footer_Menu;
+import pageObjects.Links_List;
 
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
@@ -15,7 +15,7 @@ import utility.Constant;
 import appModules.VerifyLinks_Action;
 import appModules.Navigation_Action;
 
-public class Validate_FooterMenu {
+public class Validate_Links {
     
 public WebDriver driver;
     //Test Method to start browser session
@@ -30,17 +30,18 @@ public WebDriver driver;
   public void A_Launch(){
       Navigation_Action.Navigate_To_HomePage(driver,Constant.URL);
   }
-  //Test Method to count footer menu links
+//Test Method to verify footer links 
   @Test(priority=2)
-  public void checkFooter() {
-      VerifyLinks_Action.ExecuteCount(driver);
-  } 
-//Test Method to verify all list
-  @Test(priority=3)
-  public void listAllLink() {
-      VerifyLinks_Action.ListLinks(driver, Footer_Menu.footerString(driver));
+  public void verify_FooterLinks() {
+      VerifyLinks_Action.ListLinks(driver, Links_List.footerString(driver));
+      System.out.println("Total Footer Links: " +  Links_List.footerString(driver).size());
   }
-
+//Test Method to verify all links 
+  @Test(priority=3)
+  public void verify_allLinks() {
+      VerifyLinks_Action.ListLinks(driver, Links_List.All_links(driver));
+      System.out.println("Total All Links: " +  Links_List.All_links(driver).size());
+  }
   //Test Method to close the browser session
   @AfterTest
   public void endSession() {
