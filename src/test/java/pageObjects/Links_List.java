13abcdf0a78da2d983525230cb986aa987801d81
ridho <rijuve@yahoo.com>
2017-04-25 11:41:19 +0800

    remove footer test case

--- src/test/java/pageObjects/Links_List.java
+++ src/test/java/pageObjects/Links_List.java
@@ -9,11 +9,7 @@ import org.openqa.selenium.WebElement;
 
 public class Links_List {
    
-    //Method to find cashier page title   
-    public static List<WebElement> footerString(WebDriver driver){
-        List<WebElement> element= driver.findElements(By.xpath("//*[@id='footer-menu']/div/div/div[*]/div/div[*]/ul/li[*]/a"));
-        return element;
-    }
+    //Method to find all links  
     public static List<WebElement> All_links(WebDriver driver){
         List<WebElement> element= driver.findElements(By.tagName("a"));
         return element;
--- src/test/java/testCases/Validate_Links.java
+++ src/test/java/testCases/Validate_Links.java
@@ -26,18 +26,12 @@ public WebDriver driver;
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         }
   //Test Method to launch site and print page title
-  @Test(priority=1)
+  @Test(priority=0)
   public void A_Launch(){
       Navigation_Action.Navigate_To_HomePage(driver,Constant.URL);
   }
-//Test Method to verify footer links 
-  @Test(priority=2)
-  public void verify_FooterLinks() {
-      VerifyLinks_Action.ListLinks(driver, Links_List.footerString(driver));
-      System.out.println("Total Footer Links: " +  Links_List.footerString(driver).size());
-  }
 //Test Method to verify all links 
-  @Test(priority=3)
+  @Test(priority=1)
   public void verify_allLinks() {
       VerifyLinks_Action.ListLinks(driver, Links_List.All_links(driver));
       System.out.println("Total All Links: " +  Links_List.All_links(driver).size());
