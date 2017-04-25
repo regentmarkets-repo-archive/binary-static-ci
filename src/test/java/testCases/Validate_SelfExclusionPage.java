1cf5d34c0af269078f95f04de0d6690d02ea7062
Ng Chen Hon <chenhon@binary.com>
2017-04-25 10:46:44 +0800

    changed navigation

--- src/test/java/testCases/Validate_SelfExclusionPage.java
+++ src/test/java/testCases/Validate_SelfExclusionPage.java
@@ -29,7 +29,7 @@ public class Validate_SelfExclusionPage {
     //Test Method to navigate to Self Exclusion page
     @Test(priority=1)
     public void NavigateToSelfExclusionPage() {
-    	Navigation_Action.Navigate_To_SelfExclusionPage(driver);
+    	Navigation_Action.Navigate_To_SelfExclusionPage_ValidateInput(driver);
     }
     
     @Test(priority=2)
