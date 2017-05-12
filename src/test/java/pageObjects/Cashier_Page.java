5c5ae7fa1284422b93911a992795a864a3a67a81
bijan <bijan2007@gmail.com>
2017-05-12 14:46:15 +0530

    other fixes

--- src/test/java/pageObjects/Cashier_Page.java
+++ src/test/java/pageObjects/Cashier_Page.java
@@ -13,28 +13,28 @@ public class Cashier_Page {
     //Method to find cashier page title   
     public static WebElement page_title(WebDriver driver){
         WebDriverWait wait = new WebDriverWait(driver,30);
-        element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='content']/div[2]/div/div[1]/h1")));
+        element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='content']/div[2]/h1")));
         Assert.assertTrue(element.isDisplayed());
         return element;
     }
     //Method to find cashier page sub title   
     public static WebElement sub_title(WebDriver driver){
         WebDriverWait wait = new WebDriverWait(driver,30);
-        element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='content']/div[2]/div/div[4]/div[1]/h3")));
+        element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='content']/div[2]/div[3]/h3")));
         Assert.assertTrue(element.isDisplayed());
         return element;
     }
-    //Method  find deposit button  button 
+    //Method  find deposit button
     public static WebElement deposit(WebDriver driver){
         WebDriverWait wait = new WebDriverWait(driver,30);
-        element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='content']/div[2]/div/div[4]/div[2]/div/div/div[3]/div[1]/a")));
+        element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='content']/div[2]/div[3]/div/div[3]/div[1]/a")));
         Assert.assertTrue(element.isDisplayed());
         return element;
     }
     //Method  find  withdraw button   
     public static WebElement withdraw(WebDriver driver){
         WebDriverWait wait = new WebDriverWait(driver,30);
-        element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='content']/div[2]/div/div[4]/div[2]/div/div/div[3]/div[2]/a/span")));
+        element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='content']/div[2]/div[3]/div/div[3]/div[2]/a")));
         Assert.assertTrue(element.isDisplayed());
         return element;
     }
