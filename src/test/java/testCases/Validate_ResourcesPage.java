e36c2bad1fb11fd3ec797477819fbd15d9be976b
ridho <rijuve@yahoo.com>
2017-06-09 17:50:40 +0800

    Adding check for asset index phase 1

--- src/test/java/testCases/Validate_ResourcesPage.java
+++ src/test/java/testCases/Validate_ResourcesPage.java
@@ -29,13 +29,37 @@ public class Validate_ResourcesPage {
     public void checkTitle() {
         Resources_Action.checkTitle(driver);
     }
-    //Check empty field validation
+    //Test method to check Asset Index
     @Test(priority=2)
     public void check_AssetIndex() {
         Resources_Action.link_AssetIndex(driver);
-        Resources_Action.checkMarketType(driver);
+        
     }
-    
+    //Test method to check Forex Market
+    @Test(priority=3)
+    public void check_ForexMarket() {
+        Resources_Action.clickNext(driver,0);
+        Resources_Action.checkMajorPairs(driver);
+        Resources_Action.checkMinorPairs(driver);
+        Resources_Action.checkSmartFx(driver);
+    }
+    //Test method to check Forex Market
+    @Test(priority=4)
+    public void check_IndicesMarket() {
+        Resources_Action.clickNext(driver,1);
+        Resources_Action.checkAsiaOceaniaUnderlying(driver);
+        Resources_Action.checkEuropeAfricaUnderlying(driver);
+        Resources_Action.checkOTCIndices(driver);
+        Resources_Action.checkAmericaIndices(driver);
+        Resources_Action.checkMiddleEastIndices(driver);
+    }
+    //Test method to check OTC Stocks Market
+    @Test(priority=5)
+    public void check_OTCMarket() {
+        Resources_Action.clickNext(driver,2);
+        Resources_Action.checkGermanyStocks(driver);
+    }
+      
      //Test Method to start browser session and launch site
     @BeforeTest
     public void launchApplication() {
