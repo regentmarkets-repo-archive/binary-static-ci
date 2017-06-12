22152c0deee552f9cc1a1f9a6ec106440d86ac08
bijan <bijan2007@gmail.com>
2017-06-12 20:17:06 +0530

    Add Profit Table tests

--- src/test/java/testCases/Validate_ProfitTable.java
+++ src/test/java/testCases/Validate_ProfitTable.java
@@ -5,10 +5,10 @@ import org.testng.annotations.Test;
 import utility.Constant;
 import appModules.Login_Action;
 import appModules.Navigation_Action;
-import appModules.Portfolio_Action;
+import appModules.ProfitTable_Action;
 import appModules.Trading_Action;
 
-public class Validate_PortfolioPage extends BaseClass {
+public class Validate_ProfitTable extends BaseClass{
 	
 	@Test(priority=0,description="Test Method to Login to binary site")
 	public void Login() {
@@ -26,12 +26,15 @@ public class Validate_PortfolioPage extends BaseClass {
 		Trading_Action.ValidateContractTopPurchase(driver,"Volatility 50 Index","1","h","10");
 		Trading_Action.CloseViewPopup(driver);
 	}
-	@Test(priority=3,description="Test Method to navigate to portfolio page")
-	  public void NavigateToPortfolioPage() {
-		Navigation_Action.Navigate_To_PortfolioPage(driver);
+	@Test(priority=3,description="Test Method to navigate to profit table page")
+	  public void NavigateToProfitTablePage() {
+		Navigation_Action.Navigate_To_ProfitTablePage(driver);
 	  }
-	@Test(priority=5,description="Test Method to validate sell contract")
-	  public void ValidateSellContractPortfolio() {
-		Portfolio_Action.ValidatePortfolioSellContract(driver,"Volatility 50 Index","1","h","10");
+	@Test(priority=4,description="Test Method to validate Contract Purchase View")
+	  public void ValidateContractPurchaseView() {
+		ProfitTable_Action.ValidateViewContract(driver);
+		Trading_Action.CloseViewPopup(driver);
 	  }
+	
+
 }
