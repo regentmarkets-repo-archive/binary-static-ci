524b46bfb2b91191b61c4c783ef6d296f2d06cfc
ridho <rijuve@yahoo.com>
2017-06-16 15:31:23 +0800

    Refactor common function - use base class

--- src/test/java/testCases/Validate_ResourcesPage.java
+++ src/test/java/testCases/Validate_ResourcesPage.java
@@ -1,50 +1,40 @@
-//Test case to check error validation of each field on the Financial Assessment page
 package testCases;
 
 import org.testng.annotations.Test;
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import utility.Constant;
 import appModules.Navigation_Action;
 import appModules.Resources_Action;
 import appModules.Trading_Action;
 
-public class Validate_ResourcesPage {
-    public WebDriver driver;
-    //Test Method to login into the site
-    @Test(priority=0)
-    public void Login() {
-        //Navigation_Action.Navigate_To_PlatformPage(driver);
-        //Navigation_Action.Navigate_To_TradePage(driver);
+public class Validate_ResourcesPage extends BaseClass{
+    @Test(priority=0, description="Test Method to go to trading page")
+    public void resourcePage() {
         driver.navigate().to(Constant.tradeURL);
         Trading_Action.CheckMarketOptions(driver);
         Navigation_Action.Navigate_To_ResourcesPage(driver);
     } 
-    //Test Method to navigate to cashier password page
-    @Test(priority=1)
+  
+    @Test(priority=1, description="Test Method to check resource page title")
     public void checkTitle() {
         Resources_Action.checkTitle(driver);
     }
-    //Test method to check Asset Index
-    @Test(priority=2)
+    
+    @Test(priority=2,description="Test method to check Asset Index")
     public void check_AssetIndex() {
         Resources_Action.link_AssetIndex(driver);
         
     }
-    //Test method to check Forex Market
-    @Test(priority=3)
+    
+    @Test(priority=3,description="Test method to check Forex Market")
     public void check_ForexMarket() {
+        Resources_Action.checkMarket(driver);
         Resources_Action.clickNext(driver,0);
         Resources_Action.checkMajorPairs(driver);
         Resources_Action.checkMinorPairs(driver);
         Resources_Action.checkSmartFx(driver);
     }
-    //Test method to check Forex Market
-    @Test(priority=4)
+    
+    @Test(priority=4,description="Test method to check Indices Market")
     public void check_IndicesMarket() {
         Resources_Action.clickNext(driver,1);
         Resources_Action.checkAsiaOceaniaUnderlying(driver);
@@ -53,25 +43,11 @@ public class Validate_ResourcesPage {
         Resources_Action.checkAmericaIndices(driver);
         Resources_Action.checkMiddleEastIndices(driver);
     }
-    //Test method to check OTC Stocks Market
-    @Test(priority=5)
+    
+    @Test(priority=5, description="Test method to check OTC Stocks Market")
     public void check_OTCMarket() {
         Resources_Action.clickNext(driver,2);
         Resources_Action.checkGermanyStocks(driver);
     }
       
-     //Test Method to start browser session and launch site
-    @BeforeTest
-    public void launchApplication() {
-        ChromeDriverManager.getInstance().setup();
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.get(Constant.URL);
-    
-    }
-    //Test Method to close browser session
-    @AfterTest
-    public void endSession() {
-        driver.quit();
-    }
 }
--- src/test/java/utility/ListsUtil.java
+++ src/test/java/utility/ListsUtil.java
@@ -28,165 +28,27 @@ public class ListsUtil {
         Assert.assertTrue(expectedList.containsAll(actualList));
 
     }
-    // Method to compare two list of market type
-    public static void compareMarketType(String[] marketType,List<WebElement> allMarket) {
+    // Method to compare two list of from a WebElement list
+    public static void compareWebElementList(String[] expectedList,List<WebElement> actualList) {
         //Method to check marketType  available
-        ArrayList<String> expectedMarket = new ArrayList<String>();
-        ArrayList<String> actualMarket = new ArrayList<String>();
+        ArrayList<String> newExpectedList = new ArrayList<String>();
+        ArrayList<String> newActualList = new ArrayList<String>();
 
-        for(int i = 0;i<marketType.length;i++) {
-            expectedMarket.add(marketType[i]); 
+        for(int i = 0;i<expectedList.length;i++) {
+            newExpectedList.add(expectedList[i]); 
+            System.out.println(newExpectedList);
         }
 
-        for (WebElement element: allMarket) {
-            actualMarket.add(element.getText().trim());
-        }
-        // Verify both the lists having same size
-        Assert.assertTrue(expectedMarket.size() == actualMarket.size());
-        // Compare expected and actual list
-        Assert.assertTrue(expectedMarket.containsAll(actualMarket));
-        Assert.assertEquals(actualMarket,expectedMarket);
-
-
-    }
-    // Method to compare two list of Forex - Major Pairs Underlying
-    public static void compareMajorPairsUnderlying(String[] MajorPairs,List<WebElement> MajorPairsUnderlying) {
-
-        ArrayList<String> expectedMajorPairsUnderlying = new ArrayList<String>();
-        ArrayList<String> actualMajorPairsUnderlying = new ArrayList<String>();
-
-        for(int i = 0;i<MajorPairs.length;i++) {
-            expectedMajorPairsUnderlying.add(MajorPairs[i]); 
-        }
-
-        for (WebElement element: MajorPairsUnderlying) {
-            actualMajorPairsUnderlying.add(element.getText().trim());
-
+        for (WebElement element: actualList) {
+            newActualList.add(element.getText().trim());
+            System.out.println(newActualList);
         }
         // Verify both the lists having same size
-        Assert.assertTrue(expectedMajorPairsUnderlying.size() == actualMajorPairsUnderlying.size());
+        Assert.assertTrue(newExpectedList.size() == newActualList.size());
         // Compare expected and actual list
-        Assert.assertTrue(expectedMajorPairsUnderlying.containsAll(actualMajorPairsUnderlying));
-        Assert.assertEquals(expectedMajorPairsUnderlying,actualMajorPairsUnderlying);
-    }
-
-    // Method to compare two list of Forex - Minor Pairs Underlying
-    public static void compareMinorPairsUnderlying(String[] MinorPairs,List<WebElement> MinorPairsUnderlying) {
-
-        ArrayList<String> expectedMinorPairsUnderlying = new ArrayList<String>();
-        ArrayList<String> actualMinorPairsUnderlying = new ArrayList<String>();
-
-        for(int i = 0;i<MinorPairs.length;i++) {
-            expectedMinorPairsUnderlying.add(MinorPairs[i]); 
-        }
+        Assert.assertTrue(newExpectedList.containsAll(newActualList));
+        Assert.assertEquals(newExpectedList,newActualList);
 
 
-        for (WebElement element: MinorPairsUnderlying) {
-            actualMinorPairsUnderlying.add(element.getText().trim()); 
-        }
-        // Verify both the lists having same size
-        Assert.assertTrue(expectedMinorPairsUnderlying.size() == actualMinorPairsUnderlying.size());
-        // Compare expected and actual list
-        Assert.assertTrue(expectedMinorPairsUnderlying.containsAll(actualMinorPairsUnderlying));
-        Assert.assertEquals(expectedMinorPairsUnderlying,actualMinorPairsUnderlying);
-    }
-
-    // Method to compare two list of Forex - Smart Fx Underlying
-    public static void compareSmartFxUnderlying(String[] SmartFx,List<WebElement> SmartFxUnderlying) {
-
-        ArrayList<String> expectedSmartFxUnderlying = new ArrayList<String>();
-        ArrayList<String> actualSmartFxUnderlying = new ArrayList<String>();
-
-        for(int i = 0;i<SmartFx.length;i++) {
-            expectedSmartFxUnderlying.add(SmartFx[i]); 
-        }
-
-
-        for (WebElement element: SmartFxUnderlying) {
-            actualSmartFxUnderlying.add(element.getText().trim());
-        }
-        // Verify both the lists having same size
-        Assert.assertTrue(expectedSmartFxUnderlying.size() == actualSmartFxUnderlying.size());
-        // Compare expected and actual list
-        Assert.assertTrue(expectedSmartFxUnderlying.containsAll(actualSmartFxUnderlying));
-        Assert.assertEquals(expectedSmartFxUnderlying,actualSmartFxUnderlying);
-    }
-
-    // Method to compare two list of Indices Underlying
-    public static void compareAsiaOceaniaUnderlying(String[] AsiaOceania,List<WebElement> AsiaOceaniaUnderlying) {
-        
-        ArrayList<String> expectedAsiaOceaniaUnderlying = new ArrayList<String>();
-        ArrayList<String> actualAsiaOceaniaUnderlying = new ArrayList<String>();
-
-        for(int i = 0;i<AsiaOceania.length;i++) {
-            expectedAsiaOceaniaUnderlying.add(AsiaOceania[i]); 
-        }
-
-     
-        for (WebElement element: AsiaOceaniaUnderlying) {
-            actualAsiaOceaniaUnderlying.add(element.getText().trim());
-        }
-     // Verify both the lists having same size
-        Assert.assertTrue(expectedAsiaOceaniaUnderlying.size() == actualAsiaOceaniaUnderlying.size());
-        // Compare expected and actual list
-        Assert.assertTrue(expectedAsiaOceaniaUnderlying.containsAll(actualAsiaOceaniaUnderlying));
-        Assert.assertEquals(expectedAsiaOceaniaUnderlying,actualAsiaOceaniaUnderlying);
-    }
-    // Method to compare two list of Indices Underlying
-    public static void compareEuropeAfricaUnderlying(String[] EuropeAfrica,List<WebElement> EuropeAfricaUnderlying) {
-        
-        ArrayList<String> expectedAEuropeAfricaUnderlying = new ArrayList<String>();
-        ArrayList<String> actualEuropeAfricaUnderlying = new ArrayList<String>();
-
-        for(int i = 0;i<EuropeAfrica.length;i++) {
-            actualEuropeAfricaUnderlying.add(EuropeAfrica[i]); 
-        }
-
-        for (WebElement element: EuropeAfricaUnderlying) {
-            expectedAEuropeAfricaUnderlying.add(element.getText().trim());
-        }
-     // Verify both the lists having same size
-        Assert.assertTrue(expectedAEuropeAfricaUnderlying.size() == actualEuropeAfricaUnderlying.size());
-        // Compare expected and actual list
-        Assert.assertTrue(expectedAEuropeAfricaUnderlying.containsAll(actualEuropeAfricaUnderlying));
-        Assert.assertEquals(expectedAEuropeAfricaUnderlying,actualEuropeAfricaUnderlying);
-    }
-    public static void compareOTCIndicesUnderlying(String[] OTCIndices,List<WebElement> OTCIndicesUnderlying) {
-        
-        ArrayList<String> expectedOTCIndicesUnderlying = new ArrayList<String>();
-        ArrayList<String> actualOTCIndicesUnderlying = new ArrayList<String>();
-
-        for(int i = 0;i<OTCIndices.length;i++) {
-            expectedOTCIndicesUnderlying.add(OTCIndices[i]); 
-        }
-
-        for (WebElement element: OTCIndicesUnderlying) {
-            actualOTCIndicesUnderlying.add(element.getText().trim());
-        }
-     // Verify both the lists having same size
-        Assert.assertTrue(expectedOTCIndicesUnderlying.size() == actualOTCIndicesUnderlying.size());
-        // Compare expected and actual list
-        Assert.assertTrue(expectedOTCIndicesUnderlying.containsAll(actualOTCIndicesUnderlying));
-        Assert.assertEquals(expectedOTCIndicesUnderlying,actualOTCIndicesUnderlying);
-    }
-    public static void compareGermanyStocksUnderlying(String[] GermanyStocks,List<WebElement> GermanyStocksUnderlying) {
-        
-        ArrayList<String> expectedGermanyStocksUnderlying = new ArrayList<String>();
-        ArrayList<String> actualGermanyStocksUnderlying = new ArrayList<String>();
-
-        for(int i = 0;i<GermanyStocks.length;i++) {
-            expectedGermanyStocksUnderlying.add(GermanyStocks[i]); 
-            System.out.println(expectedGermanyStocksUnderlying);
-        }
-
-        for (WebElement element: GermanyStocksUnderlying) {
-            actualGermanyStocksUnderlying.add(element.getText().trim());
-            System.out.println(actualGermanyStocksUnderlying);
-        }
-     // Verify both the lists having same size
-        Assert.assertTrue(expectedGermanyStocksUnderlying.size() == actualGermanyStocksUnderlying.size());
-        // Compare expected and actual list
-        Assert.assertTrue(expectedGermanyStocksUnderlying.containsAll(actualGermanyStocksUnderlying));
-        Assert.assertEquals(expectedGermanyStocksUnderlying,actualGermanyStocksUnderlying);
     }
 }
