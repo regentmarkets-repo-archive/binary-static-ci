449d15467bde574b60e844f53b125a1b18d54583
bijan <bijan2007@gmail.com>
2017-07-17 22:45:45 +0530

    add event listener class

--- src/test/java/testCases/BaseClass.java
+++ src/test/java/testCases/BaseClass.java
@@ -3,24 +3,17 @@ package testCases;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
 import io.github.bonigarcia.wdm.ChromeDriverManager;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
 import utility.Constant;
-import utility.WebEventListener;
 
 public class BaseClass {
 	
 	WebDriver driver;
-	public EventFiringWebDriver e_driver;
-	public WebEventListener eventListener;
 	 
 	@BeforeClass
     public void setupApplication() {
@@ -32,16 +25,13 @@ public class BaseClass {
     	ChromeOptions options = new ChromeOptions();
     	options.setExperimentalOption("prefs", prefs);
     	driver = new ChromeDriver(options);
-    	e_driver = new EventFiringWebDriver(driver);
-		eventListener = new WebEventListener();
-		e_driver.register(eventListener);
-		e_driver.manage().window().maximize();
-		e_driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		e_driver.get(Constant.URL);
+    	driver.manage().window().maximize();
+    	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    	driver.get(Constant.URL);
     	}
 	
 	  @AfterClass
 	  public void closeApplication() {
-		  e_driver.quit();
+		  driver.quit();
 	  }
 }
