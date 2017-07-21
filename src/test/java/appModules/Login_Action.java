b29b984e03117320ce14f0443ba622fe9220c984
bijan <bijan2007@gmail.com>
2017-07-21 22:46:09 +0530

    Fix Login tests

--- src/test/java/appModules/Login_Action.java
+++ src/test/java/appModules/Login_Action.java
@@ -1,7 +1,7 @@
 package appModules;
 
 import org.openqa.selenium.WebDriver;
-
+import pageObjects.AuthorisedApplications_page;
 import pageObjects.Home_Page;
 import pageObjects.Login_Page;
 import pageObjects.MainAccount_Menu;
@@ -9,16 +9,15 @@ import pageObjects.MainAccount_Menu;
 public class Login_Action {
     //Method to perform login action
     public static void Execute(WebDriver driver, String sEmail, String sPassword) {
-        //Clear the text field for Email
         Login_Page.txt_Email(driver).clear();
-        //Enter value in the Email text field
         Login_Page.txt_Email(driver).sendKeys(sEmail);
-        //Clear the text field for Password
         Login_Page.txt_Password(driver).clear();
-        //Enter value in the Password text field
         Login_Page.txt_Password(driver).sendKeys(sPassword);
-        //Click on the Login button in login page
         Login_Page.btn_Login(driver).click();
+        int num = AuthorisedApplications_page.authorize_title(driver);
+        if(num >0){
+        	AuthorisedApplications_page.grant_btn(driver).click();
+        }
     }
     public static void SwitchToVirtualAccount(WebDriver driver) {
         MainAccount_Menu.link_MainAccount(driver).click();
@@ -32,4 +31,6 @@ public class Login_Action {
         MainAccount_Menu.link_MainAccount(driver).click();
         MainAccount_Menu.link_MLTAccount(driver).click();
     }
+    
+    
 }
