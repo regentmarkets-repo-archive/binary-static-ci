d0e46eb4c722eab0bb0762879c60376d9e6404de
ehtesham <ehtesham.ahmed@live.com>
2017-07-21 12:38:11 +0800

    changes to constant.java

--- src/test/java/utility/Constant.java
+++ src/test/java/utility/Constant.java
@@ -57,7 +57,7 @@ public class Constant {
 	public static final String minPassword=RandomStringUtils.randomAlphabetic(2);
 	public static final String weakPassword=RandomStringUtils.randomAlphabetic(6);
 	public static final String inv_cashierPass="Abcd123456";
-  public static final String assetURL="https://staging.binary.com/en/resources/asset_indexws.html";
+    public static final String assetURL="https://staging.binary.com/en/resources/asset_indexws.html";
 	public static final String browser="local"; //set to "remote" for browserstack execution
 
 }
