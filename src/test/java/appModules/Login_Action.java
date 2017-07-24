f69c3a9135f328450b46f048ea9586a699c14a55
bijan <bijan2007@gmail.com>
2017-07-24 20:51:53 +0530

    Fix tests

--- src/test/java/appModules/Login_Action.java
+++ src/test/java/appModules/Login_Action.java
@@ -1,6 +1,8 @@
 package appModules;
 
 import org.openqa.selenium.WebDriver;
+
+import pageObjects.AuthorisedApplications_page;
 import pageObjects.Home_Page;
 import pageObjects.Login_Page;
 import pageObjects.MainAccount_Menu;
@@ -25,6 +27,21 @@ public class Login_Action {
     public static void SwitchToMLTAccount(WebDriver driver) {
         MainAccount_Menu.link_MainAccount(driver).click();
         MainAccount_Menu.link_MLTAccount(driver).click();
+    }
+    public static void checkAuthorizedPageAndLogin(WebDriver driver, String sEmail,String sPassword){
+       
+    	Login_Page.txt_Email(driver).clear();
+        Login_Page.txt_Email(driver).sendKeys(sEmail);
+        Login_Page.txt_Password(driver).clear();
+        Login_Page.txt_Password(driver).sendKeys(sPassword);
+        Login_Page.btn_Login(driver).click();
+      try{
+        AuthorisedApplications_page.grant_btn(driver).click();
+       }
+       catch(Exception e){
+        	System.out.println("Authorized Page is not displayed");
+        }
+    	
     }
     
     
